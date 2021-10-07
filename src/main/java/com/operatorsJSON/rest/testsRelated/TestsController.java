00a54ae9448d5d48f5706ffa1efb3a061a520a7e
Subuday77 <subuday.bagatur@gmail.com>
2021-10-07 12:16:51 +0300

    Fixing mistype "files" to "file"

--- src/main/java/com/operatorsJSON/rest/testsRelated/TestsController.java
+++ src/main/java/com/operatorsJSON/rest/testsRelated/TestsController.java
@@ -136,7 +136,8 @@ public class TestsController {
             return new ResponseEntity<String>("Access deny", HttpStatus.FORBIDDEN);
         }
         String fileName = operatorId + "_Test_Log.log";
-        File file = new File("files\\" + fileName);
+        File file = new File("file\\" + fileName);
+        System.out.println(file);
         if (file.exists()) {
             Path path = Paths.get(file.getAbsolutePath());
             ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(path));
