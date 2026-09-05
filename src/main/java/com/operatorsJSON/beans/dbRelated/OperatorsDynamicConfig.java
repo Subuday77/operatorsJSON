20352705a813ed05f3f2dcbdcf6cade6bf88b8ca
Subuday77 <55333225+Subuday77@users.noreply.github.com>
2026-09-05 14:49:06 +0300

    Redact runtime tokens from logs

--- src/main/java/com/operatorsJSON/beans/dbRelated/OperatorsDynamicConfig.java
+++ src/main/java/com/operatorsJSON/beans/dbRelated/OperatorsDynamicConfig.java
@@ -1,6 +1,5 @@
 package com.operatorsJSON.beans.dbRelated;
 
-
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -27,8 +26,7 @@ public class OperatorsDynamicConfig {
     private boolean tokenUsed;
     private boolean onlyWholeNumbers;
 
-    public OperatorsDynamicConfig() {
-    }
+    public OperatorsDynamicConfig() {}
 
     public OperatorsDynamicConfig(long belongsToOperator, long startingRound, int delay, String initialToken, String sessionToken, String uid, String currency, double initialBalance, double basicBetAmount, boolean tokenUsed, boolean onlyWholeNumbers) {
         this.belongsToOperator = belongsToOperator;
@@ -44,104 +42,36 @@ public class OperatorsDynamicConfig {
         this.onlyWholeNumbers = onlyWholeNumbers;
     }
 
-    @Id
-    @Column  //(nullable = false, updatable = false)
-    public long getBelongsToOperator() {
-        return belongsToOperator;
-    }
+    @Id @Column public long getBelongsToOperator() { return belongsToOperator; }
+    public void setBelongsToOperator(long belongsToOperator) { this.belongsToOperator = belongsToOperator; }
+    @Column public long getStartingRound() { return startingRound; }
+    public void setStartingRound(long startingRound) { this.startingRound = startingRound; }
+    @Column public int getDelay() { return delay; }
+    public void setDelay(int delay) { this.delay = delay; }
+    @Column public String getInitialToken() { return initialToken; }
+    public void setInitialToken(String initialToken) { this.initialToken = initialToken; }
+    @Column public String getSessionToken() { return sessionToken; }
+    public void setSessionToken(String sessionToken) { this.sessionToken = sessionToken; }
+    @Column public String getUid() { return uid; }
+    public void setUid(String uid) { this.uid = uid; }
+    @Column public String getCurrency() { return currency; }
+    public void setCurrency(String currency) { this.currency = currency; }
+    @Column public double getInitialBalance() { return initialBalance; }
+    public void setInitialBalance(double initialBalance) { this.initialBalance = initialBalance; }
+    @Column public double getBasicBetAmount() { return basicBetAmount; }
+    public void setBasicBetAmount(double basicBetAmount) { this.basicBetAmount = basicBetAmount; }
+    @Column public boolean isTokenUsed() { return tokenUsed; }
+    public void setTokenUsed(boolean tokenUsed) { this.tokenUsed = tokenUsed; }
+    @Column public boolean isOnlyWholeNumbers() { return onlyWholeNumbers; }
+    public void setOnlyWholeNumbers(boolean onlyWholeNumbers) { this.onlyWholeNumbers = onlyWholeNumbers; }
 
-    public void setBelongsToOperator(long belongsToOperator) {
-        this.belongsToOperator = belongsToOperator;
-    }
-    @Column
-    public long getStartingRound() {
-        return startingRound;
-    }
-
-    public void setStartingRound(long startingRound) {
-        this.startingRound = startingRound;
-    }
-    @Column
-    public int getDelay() {
-        return delay;
-    }
-
-    public void setDelay(int delay) {
-        this.delay = delay;
-    }
-    @Column
-    public String getInitialToken() {
-        return initialToken;
-    }
-
-    public void setInitialToken(String initialToken) {
-        this.initialToken = initialToken;
-    }
-    @Column
-    public String getSessionToken() {
-        return sessionToken;
-    }
-
-    public void setSessionToken(String sessionToken) {
-        this.sessionToken = sessionToken;
-    }
-    @Column
-    public String getUid() {
-        return uid;
-    }
-
-    public void setUid(String uid) {
-        this.uid = uid;
-    }
-    @Column
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-    @Column
-    public double getInitialBalance() {
-        return initialBalance;
-    }
-
-    public void setInitialBalance(double initialBalance) {
-        this.initialBalance = initialBalance;
-    }
-    @Column
-    public double getBasicBetAmount() {
-        return basicBetAmount;
-    }
-
-    public void setBasicBetAmount(double basicBetAmount) {
-        this.basicBetAmount = basicBetAmount;
-    }
-    @Column
-    public boolean isTokenUsed() {
-        return tokenUsed;
-    }
-
-    public void setTokenUsed(boolean tokenUsed) {
-        this.tokenUsed = tokenUsed;
-    }
-    @Column
-    public boolean isOnlyWholeNumbers() {
-        return onlyWholeNumbers;
-    }
-
-    public void setOnlyWholeNumbers(boolean onlyWholeNumbers) {
-        this.onlyWholeNumbers = onlyWholeNumbers;
-    }
-
-    @Override
-    public String toString() {
+    @Override public String toString() {
         return "OperatorsDynamicConfig{" +
                 "belongsToOperator=" + belongsToOperator +
                 ", startingRound=" + startingRound +
                 ", delay=" + delay +
-                ", initialToken='" + initialToken + '\'' +
-                ", sessionToken='" + sessionToken + '\'' +
+                ", initialToken='[REDACTED]'" +
+                ", sessionToken='[REDACTED]'" +
                 ", uid='" + uid + '\'' +
                 ", currency='" + currency + '\'' +
                 ", initialBalance=" + initialBalance +
